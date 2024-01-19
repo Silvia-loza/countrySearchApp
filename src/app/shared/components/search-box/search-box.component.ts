4c5ff1b86c39e57f6aaa2006a6d24272a8a3cc73
Silvia Lozano <sylvilozano@gmail.com>
2024-01-19 11:22:52 +0100

    refactor: added unsubscribe for performance improvements in debouncer subscription

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,24 +1,39 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.scss',
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscirption?: Subscription;
 
   @Input()
   placeholder: string = '';
   @Output()
   public onDebounce: EventEmitter<string> = new EventEmitter();
   ngOnInit(): void {
-    this.debouncer.pipe(debounceTime(300)).subscribe((searchWord) => {
-      this.onDebounce.emit(searchWord);
-    });
+    this.debouncerSubscirption = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((searchWord) => {
+        this.onDebounce.emit(searchWord);
+      });
   }
   onKeyPress(searchWord: string) {
     this.debouncer.next(searchWord);
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscirption?.unsubscribe();
+    console.log('destruido');
   }
 }
