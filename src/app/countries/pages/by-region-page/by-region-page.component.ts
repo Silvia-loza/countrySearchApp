8e61cb3bfbc86034b43fdffe4213fc3f94df4c49
Silvia Lozano <sylvilozano@gmail.com>
2024-01-19 11:54:51 +0100

    feature: add search by Region restrictions as limited options

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
-
+type Region = 'Americas' | 'Africa' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: ``,
+  styles: `
+  .btn {
+    width: 100%;
+  }
+  .btn-outline-dark {
+    border-color: #f699cd;
+  }`,
 })
 export class ByRegionPageComponent {
   constructor(private countriesService: CountriesService) {}
   public placeHolder: string = 'Buscar Región...';
   public countryList: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = [
+    'Americas',
+    'Africa',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
-  SearchByRegion(searchWord: string): void {
+  SearchByRegion(searchWord: Region): void {
     this.isLoading = true;
+    this.selectedRegion = searchWord;
 
     this.countriesService.searchByRegion(searchWord).subscribe((countries) => {
       this.countryList = countries;
